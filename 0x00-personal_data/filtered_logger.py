089bfcba41b6c39bfe5267a28dc3dbc607642744
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-11-03 00:05:09 +0300

    filtered_logger.py - Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,7 +5,8 @@ Module for handling Personal Data
 import re
 from typing import List
 import logging
-import os
+from os import environ
+from os
 import mysql.connector
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
@@ -72,16 +73,17 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         A MySQLConnection object representing the established
           database connection.
     """
-    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
-    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
-    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
+    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = environ.get("PERSONAL_DATA_DB_NAME")
 
-    db = mysql.connector.connect(
-        user=username, password=password, host=host, database=db_name
-    )
-
+    db = mysql.connector.connection.MySQLConnection(user=username,
+                                                     password=password,
+                                                     host=host,
+                                                     database=db_name)
     return db
+
 
 
 class RedactingFormatter(logging.Formatter):
