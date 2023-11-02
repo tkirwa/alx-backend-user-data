23c7a62c51060ff27d31cd87076326984e764f3c
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-11-02 23:42:37 +0300

    get_logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -28,10 +28,31 @@ def filter_datum(
     return message
 
 
+def get_logger() -> logging.Logger:
+    """
+    Creates and returns a logger object with specific settings.
+
+    The logger object is named "user_data" and its level is set to INFO. 
+    Propagation is turned off to prevent the log messages from being passed 
+    to the root logger. A stream handler with a specific formatter is added 
+    to the logger. This formatter obfuscates fields defined in PII_FIELDS.
+
+    Returns:
+        logging.Logger: The configured logger object.
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(stream_handler)
+
+    return logger
+
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """Redacting Formatter class"""
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
@@ -42,11 +63,13 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """ Filters values in incoming log records using filter_datum """
-        record.msg = filter_datum(self.fields, self.REDACTION,
-                                  record.getMessage(), self.SEPARATOR)
+        """Filters values in incoming log records using filter_datum"""
+        record.msg = filter_datum(
+            self.fields, self.REDACTION,
+            record.getMessage(),self.SEPARATOR
+        )
         return super(RedactingFormatter, self).format(record)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
