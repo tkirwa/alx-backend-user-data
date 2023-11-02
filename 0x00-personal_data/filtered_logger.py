e6f25ac3149e981f6e5043721a759edb6cfa5fa2
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-11-02 23:19:06 +0300

    Regex-ing - filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,7 +9,18 @@ from typing import List
 def filter_datum(
     fields: List[str], redaction: str, message: str, separator: str
 ) -> str:
-    """docstring"""
+    """
+    Obfuscates specified fields in a message.
+
+    Arguments:
+    fields -- List of field names that should be obfuscated.
+    redaction -- The value that should replace the obfuscated fields.
+    message -- The message that contains the fields.
+    separator -- The character that separates fields in the message.
+
+    Returns:
+    The message with the specified fields obfuscated.
+    """
     for f in fields:
         message = re.sub(f"{f}=.*?{separator}", f"{f}={redaction}{separator}", message)
     return message
