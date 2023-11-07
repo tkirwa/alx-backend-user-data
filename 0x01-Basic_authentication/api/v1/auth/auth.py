329ef23989edd988d9230bfc89e97e6aabbf6c59
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-11-08 02:50:08 +0300

    Require auth with stars

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,34 +13,28 @@ class Auth:
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-    """
-    Method to require authorization
-    Args:
-        path (str): The path of the request
-        excluded_paths (List[str]): List of excluded paths
-    Returns:
-        bool: True if path is not in excluded_paths, False otherwise
-    """
-    if path is None:
-        return True
+        """
+        Method to require authorization
+        Args:
+            path (str): The path of the request
+            excluded_paths (List[str]): List of excluded paths
+        Returns:
+            bool: True if path is not in excluded_paths, False otherwise
+        """
+        if path is None:
+            return True
 
-    if excluded_paths is None or len(excluded_paths) == 0:
-        return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
 
-    # Add a trailing slash to path if not present for uniformity
-    if path[-1] != "/":
-        path += "/"
+        # Add a trailing slash to path if not present for uniformity
+        if path[-1] != "/":
+            path += "/"
 
-    for exc_path in excluded_paths:
-        # If the excluded path ends with '*', check if the start of the path matches
-        if exc_path[-1] == "*":
-            if path.startswith(exc_path[:-1]):
-                return False
-        elif path == exc_path:
+        if path in excluded_paths:
             return False
 
-    return True
-
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
