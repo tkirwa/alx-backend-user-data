2088931d0b0746120b8ad43b09e51c54b7ffa25b
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-11-08 02:53:42 +0300

    Require auth with stars

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,6 +5,7 @@ Auth class
 """
 from flask import request
 from typing import List, TypeVar
+import fnmatch
 
 
 class Auth:
@@ -14,25 +15,22 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-        Method to require authorization
+        Method to check if authentication is required for a given path.
         Args:
             path (str): The path of the request
             excluded_paths (List[str]): List of excluded paths
         Returns:
-            bool: True if path is not in excluded_paths, False otherwise
+            bool: True if authentication is required, False otherwise
         """
         if path is None:
             return True
 
-        if excluded_paths is None or len(excluded_paths) == 0:
+        if excluded_paths is None or not excluded_paths:
             return True
 
-        # Add a trailing slash to path if not present for uniformity
-        if path[-1] != "/":
-            path += "/"
-
-        if path in excluded_paths:
-            return False
+        for excluded_path in excluded_paths:
+            if fnmatch.fnmatch(path, excluded_path):
+                return False
 
         return True
 
