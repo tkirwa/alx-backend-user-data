e39af692ab5c240d333ecfc83be7b3afd4327c44
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-11-08 17:49:38 +0300

    update basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -3,12 +3,12 @@
 Route module for the API
 """
 from os import getenv
-from api.v1.views import app_views
+
 from flask import Flask, jsonify, abort, request
-from flask_cors import CORS, cross_origin
-import os
+from flask_cors import CORS
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
