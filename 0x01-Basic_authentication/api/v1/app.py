79ea5e82a26d997d04a6353c9a4f1a42e24b4675
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-11-09 11:35:04 +0300

    status fix

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
+"""Route module for the API.
 """
-Route module for the API
-"""
+# import os
 from os import getenv
-
 from flask import Flask, jsonify, abort, request
 from flask_cors import CORS
+
+from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
-from api.v1.views import app_views
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
 auth = None
 auth_type = getenv("AUTH_TYPE", "auth")
 if auth_type == "auth":
@@ -24,19 +24,19 @@ if auth_type == "basic_auth":
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """Not found handler"""
+    """Not found handler."""
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
-def unauthorized_error(error) -> str:
-    """Unauthorized handler"""
+def unauthorized(error) -> str:
+    """Unauthorized handler."""
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden_error(error) -> str:
-    """Forbidden handler"""
+def forbidden(error) -> str:
+    """Forbidden handler."""
     return jsonify({"error": "Forbidden"}), 403
 
 
