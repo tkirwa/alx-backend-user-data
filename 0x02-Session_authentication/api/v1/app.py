c0e13e3c0d46db375757b8bd5820ef1e224ec89b
Tonny Kirwa <118567738+tkirwa@users.noreply.github.com>
2023-11-10 02:18:27 +0300

    Update app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -1,75 +1,72 @@
 #!/usr/bin/env python3
-"""Route module for the API.
 """
-# import os
+Route module for the API
+"""
 from os import getenv
-from flask import Flask, jsonify, abort, request
-from flask_cors import CORS
-
 from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
-from api.v1.auth.session_db_auth import SessionDBAuth
 from api.v1.auth.session_exp_auth import SessionExpAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-auth_type = getenv("AUTH_TYPE", "auth")
-if auth_type == "auth":
-    auth = Auth()
-if auth_type == "basic_auth":
+if os.getenv("AUTH_TYPE") == "basic_auth":
     auth = BasicAuth()
-if auth_type == "session_auth":
+elif os.getenv("AUTH_TYPE") == "session_auth":
     auth = SessionAuth()
-if auth_type == "session_exp_auth":
+elif os.getenv("AUTH_TYPE") == "session_exp_auth":
     auth = SessionExpAuth()
-if auth_type == "session_db_auth":
+elif os.getenv("AUTH_TYPE") == "session_db_auth":
     auth = SessionDBAuth()
-
-
-@app.before_request
-def bef_req():
-    """
-    Filter each request before it's handled by the proper route
-    """
-    if auth is None:
-        pass
-    else:
-        setattr(request, "current_user", auth.current_user(request))
-        excluded = [
-            "/api/v1/status/",
-            "/api/v1/unauthorized/",
-            "/api/v1/forbidden/",
-            "/api/v1/auth_session/login/",
-        ]
-        if auth.require_auth(request.path, excluded):
-            cookie = auth.session_cookie(request)
-            if auth.authorization_header(request) is None and cookie is None:
-                abort(401, description="Unauthorized")
-            if auth.current_user(request) is None:
-                abort(403, description="Forbidden")
+elif os.getenv("AUTH_TYPE") == "auth":
+    auth = Auth()
 
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """Not found handler"""
+    """ Not found handler
+    """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """Request unauthorized handler"""
+    """
+    Unauthorized handler.
+    """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """Request unauthorized handler"""
+    """ Forbidden handler.
+    """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before():
+    """ Before request.
+    """
+    if auth:
+        paths = ['/api/v1/status/',
+                 '/api/v1/unauthorized/', '/api/v1/forbidden/',
+                 '/api/v1/auth_session/login/']
+        if not auth.require_auth(request.path, paths):
+            return
+        if (not auth.authorization_header(request) and
+                not auth.session_cookie(request)):
+            abort(401)
+        request.current_user = auth.current_user(
+            request) if auth.current_user(request) else abort(403)
 
 
 if __name__ == "__main__":
