0362828da9d386f7a28383d8cc985d3b1068901c
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-11-09 13:24:37 +0300

    add a new endpoint: GET /users/me

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -58,6 +58,12 @@ def authenticate_user():
                 abort(403)
 
 
+@app.before_request
+def before_request_func():
+    if auth is not None:
+        request.current_user = auth.current_user(request)
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
