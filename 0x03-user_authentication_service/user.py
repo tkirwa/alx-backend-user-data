2f5838615c6d7bfd8a28abb8ca8ab958372059e8
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-11-16 11:58:11 +0300

    Update User model in user.py

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,19 +1,21 @@
+#!/usr/bin/env python3
+"""
+Define User model
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class User(Base):
     """
-    User model for the users table in the database.
+    User model
     """
-    __tablename__ = 'users'
 
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
-
-    def __repr__(self):
-        return f'<User(id={self.id}, email={self.email}, hashed_password={self.hashed_password}, session_id={self.session_id}, reset_token={self.reset_token})>'
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
