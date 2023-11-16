1fe66b0280a09d3cd6a07bef094f4b8a4278297b
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-11-16 13:48:13 +0300

    implement the Auth.update_password method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -92,5 +92,17 @@ class Auth:
             reset_token = _generate_uuid()
             self._db.update_user(user.id, reset_token=reset_token)
             return reset_token
-        except NoResultFound:
-            raise ValueError("User does not exist")
+        except NoResultFound as exc:
+            raise ValueError("User does not exist") from exc
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """
+        Update a user's password
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_password = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=hashed_password,
+                                 reset_token=None)
+        except NoResultFound as exc:
+            raise ValueError("Invalid reset token") from exc
